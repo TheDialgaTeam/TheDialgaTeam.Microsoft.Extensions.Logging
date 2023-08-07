1a70a29c08f8b3e3a5b579821eb1eaeca7f2b0fb
Yong Jian Ming <jianming1993@gmail.com>
2023-08-07 17:19:18 +0800

    Lets use this record struct which seems better?

--- src/LoggingTemplateEntry.cs
+++ src/LoggingTemplateEntry.cs
@@ -2,25 +2,4 @@
 
 namespace TheDialgaTeam.Microsoft.Extensions.Logging;
 
-public struct LoggingTemplateEntry
-{
-    /// <summary>Gets the LogLevel</summary>
-    public LogLevel LogLevel { get; }
-
-    /// <summary>Gets the log category</summary>
-    public string Category { get; }
-
-    /// <summary>Gets the log EventId</summary>
-    public EventId EventId { get; }
-
-    /// <summary>Gets the log exception</summary>
-    public Exception? Exception { get; }
-
-    public LoggingTemplateEntry(LogLevel logLevel, string category, EventId eventId, Exception? exception)
-    {
-        LogLevel = logLevel;
-        Category = category;
-        EventId = eventId;
-        Exception = exception;
-    }
-}
+public readonly record struct LoggingTemplateEntry(LogLevel LogLevel, string Category, EventId EventId, Exception? Exception);
